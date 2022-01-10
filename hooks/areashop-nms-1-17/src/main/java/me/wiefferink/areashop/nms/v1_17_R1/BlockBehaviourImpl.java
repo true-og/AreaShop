4a7a4ca4ee4e94746cd2c9cca653ccaaca63fbb4
md5sha256 <42793301+md5sha256@users.noreply.github.com>
2022-01-10 22:34:35 +0800

    Fix reobf

--- hooks/areashop-nms-1-17/src/main/java/me/wiefferink/areashop/nms/v1_17_R1/BlockBehaviourImpl.java
+++ hooks/areashop-nms-1-17/src/main/java/me/wiefferink/areashop/nms/v1_17_R1/BlockBehaviourImpl.java
@@ -1,12 +1,11 @@
 package me.wiefferink.areashop.nms.v1_17_R1;
 
 import me.wiefferink.areashop.nms.BlockBehaviourHelper;
-import net.minecraft.core.BlockPosition;
-import net.minecraft.world.level.World;
-import net.minecraft.world.level.block.state.IBlockData;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.block.state.BlockState;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockState;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.craftbukkit.v1_17_R1.CraftWorld;
 import org.bukkit.craftbukkit.v1_17_R1.block.CraftBlock;
@@ -20,28 +19,28 @@ public class BlockBehaviourImpl implements BlockBehaviourHelper {
     @Override
     public boolean canPlace(Location location, BlockData blockData) {
         Objects.requireNonNull(location.getWorld(), "Null World!");
-        final BlockPosition blockPosition = new BlockPosition(location.getX(), location.getY(), location.getZ());
+        final BlockPos blockPos = new BlockPos(location.getX(), location.getY(), location.getZ());
         final CraftWorld craftWorld = (CraftWorld) location.getWorld();
-        final World world = craftWorld.getHandle();
+        final ServerLevel world = craftWorld.getHandle();
         final CraftBlockData craftBlockData = (CraftBlockData) blockData;
-        final IBlockData ibd = craftBlockData.getState();
-        return ibd.canPlace(world, blockPosition);
+        final BlockState ibd = craftBlockData.getState();
+        return ibd.canSurvive(world, blockPos);
     }
 
     @Override
     public boolean isBlockValid(Block block) {
         final CraftBlock craftBlock = (CraftBlock) block;
         final CraftWorld craftWorld = (CraftWorld) craftBlock.getWorld();
-        final World world = craftWorld.getHandle();
-        final IBlockData ibd = craftBlock.getNMS();
-        return ibd.canPlace(world, craftBlock.getPosition());
+        final ServerLevel world = craftWorld.getHandle();
+        final BlockState ibd = craftBlock.getNMS();
+        return ibd.canSurvive(world, craftBlock.getPosition());
     }
 
     @Override
-    public boolean isBlockStateValid(BlockState blockState) {
+    public boolean isBlockStateValid(org.bukkit.block.BlockState blockState) {
         final CraftBlockState craftBlockState = (CraftBlockState) blockState;
-        final IBlockData ibd = craftBlockState.getHandle();
-        return ibd.canPlace(craftBlockState.getBlock().getCraftWorld().getHandle(), craftBlockState.getPosition());
+        final BlockState ibd = craftBlockState.getHandle();
+        return ibd.canSurvive(craftBlockState.getBlock().getCraftWorld().getHandle(), craftBlockState.getPosition());
     }
 
 }
