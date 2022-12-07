2899b2978c9f60b7cdb59eb8c6e40c46098b1eb7
Andrew Wong <42793301+md5sha256@users.noreply.github.com>
2022-12-07 11:56:50 +0800

    Rewrite SetownerCommand

--- AreaShop/src/main/java/me/wiefferink/areashop/commands/SetownerCommand.java
+++ AreaShop/src/main/java/me/wiefferink/areashop/commands/SetownerCommand.java
@@ -1,10 +1,12 @@
 package me.wiefferink.areashop.commands;
 
 import me.wiefferink.areashop.MessageBridge;
+import me.wiefferink.areashop.adapters.platform.OfflinePlayerHelper;
 import me.wiefferink.areashop.managers.IFileManager;
 import me.wiefferink.areashop.regions.BuyRegion;
 import me.wiefferink.areashop.regions.GeneralRegion;
 import me.wiefferink.areashop.regions.RentRegion;
+import me.wiefferink.areashop.tools.BukkitSchedulerExecutor;
 import me.wiefferink.areashop.tools.Utils;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
@@ -21,116 +23,116 @@ import java.util.UUID;
 @Singleton
 public class SetownerCommand extends CommandAreaShop {
 
-	@Inject
-	private MessageBridge messageBridge;
-	@Inject
-	private IFileManager fileManager;
-	
-	@Override
-	public String getCommandStart() {
-		return "areashop setowner";
-	}
+    @Inject
+    private MessageBridge messageBridge;
+    @Inject
+    private IFileManager fileManager;
+    @Inject
+    private OfflinePlayerHelper offlinePlayerHelper;
+    @Inject
+    private BukkitSchedulerExecutor executor;
 
-	@Override
-	public String getHelp(CommandSender target) {
-		if(target.hasPermission("areashop.setownerrent") || target.hasPermission("areashop.setownerbuy")) {
-			return "help-setowner";
-		}
-		return null;
-	}
+    @Override
+    public String getCommandStart() {
+        return "areashop setowner";
+    }
 
-	@Override
-	public void execute(CommandSender sender, String[] args) {
-		if(!sender.hasPermission("areashop.setownerrent") && !sender.hasPermission("areashop.setownerbuy")) {
-			messageBridge.message(sender, "setowner-noPermission");
-			return;
-		}
-		GeneralRegion region;
-		if(args.length < 2) {
-			messageBridge.message(sender, "setowner-help");
-			return;
-		}
-		if(args.length == 2) {
-			if(sender instanceof Player) {
-				// get the region by location
-				List<GeneralRegion> regions = Utils.getImportantRegions(((Player)sender).getLocation());
-				if(regions.isEmpty()) {
-					messageBridge.message(sender, "cmd-noRegionsAtLocation");
-					return;
-				} else if(regions.size() > 1) {
-					messageBridge.message(sender, "cmd-moreRegionsAtLocation");
-					return;
-				} else {
-					region = regions.get(0);
-				}
-			} else {
-				messageBridge.message(sender, "cmd-automaticRegionOnlyByPlayer");
-				return;
-			}
-		} else {
-			region = fileManager.getRegion(args[2]);
-		}
-		if(region == null) {
-			messageBridge.message(sender, "setowner-notRegistered");
-			return;
-		}
+    @Override
+    public String getHelp(CommandSender target) {
+        if (target.hasPermission("areashop.setownerrent") || target.hasPermission("areashop.setownerbuy")) {
+            return "help-setowner";
+        }
+        return null;
+    }
 
-		if(region instanceof RentRegion && !sender.hasPermission("areashop.setownerrent")) {
-			messageBridge.message(sender, "setowner-noPermissionRent", region);
-			return;
-		}
-		if(region instanceof BuyRegion && !sender.hasPermission("areashop.setownerbuy")) {
-			messageBridge.message(sender, "setowner-noPermissionBuy", region);
-			return;
-		}
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+        if (!sender.hasPermission("areashop.setownerrent") && !sender.hasPermission("areashop.setownerbuy")) {
+            messageBridge.message(sender, "setowner-noPermission");
+            return;
+        }
+        GeneralRegion region;
+        if (args.length < 2) {
+            messageBridge.message(sender, "setowner-help");
+            return;
+        }
+        if (args.length == 2) {
+            if (!(sender instanceof Player player)) {
+                messageBridge.message(sender, "cmd-automaticRegionOnlyByPlayer");
+                return;
+            }
+            // get the region by location
+            List<GeneralRegion> regions = Utils.getImportantRegions(player.getLocation());
+            if (regions.isEmpty()) {
+                messageBridge.message(sender, "cmd-noRegionsAtLocation");
+                return;
+            } else if (regions.size() > 1) {
+                messageBridge.message(sender, "cmd-moreRegionsAtLocation");
+                return;
+            } else {
+                region = regions.get(0);
+            }
 
-		UUID uuid = null;
-		@SuppressWarnings("deprecation")
-		OfflinePlayer player = Bukkit.getOfflinePlayer(args[1]);
-		if(player != null) {
-			uuid = player.getUniqueId();
-		}
-		if(uuid == null) {
-			messageBridge.message(sender, "setowner-noPlayer", args[1], region);
-			return;
-		}
+        } else {
+            region = fileManager.getRegion(args[2]);
+        }
+        if (region == null) {
+            messageBridge.message(sender, "setowner-notRegistered");
+            return;
+        }
 
-		if(region instanceof RentRegion rent) {
-			if(rent.isRenter(uuid)) {
-				// extend
-				rent.setRentedUntil(rent.getRentedUntil() + rent.getDuration());
-				rent.setRenter(uuid);
-				messageBridge.message(sender, "setowner-succesRentExtend", region);
-			} else {
-				// change
-				if(!rent.isRented()) {
-					rent.setRentedUntil(Calendar.getInstance().getTimeInMillis() + rent.getDuration());
-				}
-				rent.setRenter(uuid);
-				messageBridge.message(sender, "setowner-succesRent", region);
-			}
-		}
-		if(region instanceof BuyRegion buy) {
-			buy.setBuyer(uuid);
-			messageBridge.message(sender, "setowner-succesBuy", region);
-		}
-		region.getFriendsFeature().deleteFriend(region.getOwner(), null);
-		region.update();
-		region.saveRequired();
-	}
+        if (region instanceof RentRegion && !sender.hasPermission("areashop.setownerrent")) {
+            messageBridge.message(sender, "setowner-noPermissionRent", region);
+            return;
+        }
+        if (region instanceof BuyRegion && !sender.hasPermission("areashop.setownerbuy")) {
+            messageBridge.message(sender, "setowner-noPermissionBuy", region);
+            return;
+        }
 
-	@Override
-	public List<String> getTabCompleteList(int toComplete, String[] start, CommandSender sender) {
-		ArrayList<String> result = new ArrayList<>();
-		if(toComplete == 2) {
-			for(Player player : Utils.getOnlinePlayers()) {
-				result.add(player.getName());
-			}
-		} else if(toComplete == 3) {
-			result.addAll(fileManager.getRegionNames());
-		}
-		return result;
-	}
+        this.offlinePlayerHelper.lookupUuidAsync(args[1]).thenAcceptAsync(optionalUuid -> {
+            if (optionalUuid.isEmpty()) {
+                messageBridge.message(sender, "setowner-noPlayer", args[1], region);
+                return;
+            }
+            final UUID uuid = optionalUuid.get();
+            if (region instanceof RentRegion rent) {
+                if (rent.isRenter(uuid)) {
+                    // extend
+                    rent.setRentedUntil(rent.getRentedUntil() + rent.getDuration());
+                    rent.setRenter(uuid);
+                    messageBridge.message(sender, "setowner-succesRentExtend", region);
+                } else {
+                    // change
+                    if (!rent.isRented()) {
+                        rent.setRentedUntil(Calendar.getInstance().getTimeInMillis() + rent.getDuration());
+                    }
+                    rent.setRenter(uuid);
+                    messageBridge.message(sender, "setowner-succesRent", region);
+                }
+            }
+            if (region instanceof BuyRegion buy) {
+                buy.setBuyer(uuid);
+                messageBridge.message(sender, "setowner-succesBuy", region);
+            }
+            region.getFriendsFeature().deleteFriend(region.getOwner(), null);
+            region.update();
+            region.saveRequired();
+        }, this.executor);
+    }
+
+    @Override
+    public List<String> getTabCompleteList(int toComplete, String[] start, CommandSender sender) {
+        ArrayList<String> result = new ArrayList<>();
+        if (toComplete == 2) {
+            for (Player player : Utils.getOnlinePlayers()) {
+                result.add(player.getName());
+            }
+        } else if (toComplete == 3) {
+            result.addAll(fileManager.getRegionNames());
+        }
+        return result;
+    }
 }
 
 
