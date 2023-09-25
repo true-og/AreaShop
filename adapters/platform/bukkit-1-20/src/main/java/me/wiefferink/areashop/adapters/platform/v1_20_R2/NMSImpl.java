4d7f00b0ce221d810828ed2f5b057b0cfa0c6869
Andrew Wong <42793301+md5sha256@users.noreply.github.com>
2023-09-25 11:33:02 +1000

    Add support for 1.20.2

---
+++ adapters/platform/bukkit-1-20/src/main/java/me/wiefferink/areashop/adapters/platform/v1_20_R2/NMSImpl.java
@@ -0,0 +1,15 @@
+package me.wiefferink.areashop.adapters.platform.v1_20_R2;
+
+import me.wiefferink.areashop.nms.BlockBehaviourHelper;
+import me.wiefferink.areashop.nms.NMS;
+
+public class NMSImpl implements NMS {
+
+    private final BlockBehaviourImpl blockBehaviourHelper = new BlockBehaviourImpl();
+
+    @Override
+    public BlockBehaviourHelper blockBehaviourHelper() {
+        return this.blockBehaviourHelper;
+    }
+
+}
