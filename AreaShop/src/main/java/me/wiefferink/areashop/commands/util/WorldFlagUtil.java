5b16f40335485e88b8ffd6b1b000b350e7af78ca
Andrew Wong <42793301+md5sha256@users.noreply.github.com>
2024-03-01 21:58:45 +1100

    Refactor to use cloud keys and flags

---
+++ AreaShop/src/main/java/me/wiefferink/areashop/commands/util/WorldFlagUtil.java
@@ -0,0 +1,38 @@
+package me.wiefferink.areashop.commands.util;
+
+import org.bukkit.World;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.incendo.cloud.bukkit.parser.WorldParser;
+import org.incendo.cloud.context.CommandContext;
+import org.incendo.cloud.parser.flag.CommandFlag;
+
+public final class WorldFlagUtil {
+
+    private WorldFlagUtil() {
+        throw new IllegalStateException("Cannot instantiate static util class");
+    }
+
+    public static final CommandFlag<World> DEFAULT_WORLD_FLAG = CommandFlag.builder("world")
+            .withComponent(WorldParser.worldParser())
+            .build();
+
+    @NonNull
+    public static <C extends Entity & CommandSender> World parseOrDetectWorld(@NonNull CommandContext<C> context) {
+        return parseOrDetectWorld(context, DEFAULT_WORLD_FLAG);
+    }
+
+    @NonNull
+    public static <C extends Entity & CommandSender> World parseOrDetectWorld(
+            @NonNull CommandContext<C> context,
+            @NonNull CommandFlag<World> flag
+    ) {
+        World world = context.flags().get(flag);
+        if (world != null) {
+            return world;
+        }
+        return context.sender().getWorld();
+    }
+
+}
