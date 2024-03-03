3c21ac1eba561161e280822ea2450621e01697d5
Andrew Wong <42793301+md5sha256@users.noreply.github.com>
2024-03-04 01:15:36 +1100

    Refactor info command to cloud

--- AreaShop/src/main/java/me/wiefferink/areashop/commands/cloud/AreashopCloudCommands.java
+++ AreaShop/src/main/java/me/wiefferink/areashop/commands/cloud/AreashopCloudCommands.java
@@ -43,6 +43,7 @@ public class AreashopCloudCommands {
             GroupDelCloudCommand.class,
             GroupInfoCloudCommand.class,
             GroupListCloudCommand.class,
+            InfoCloudCommand.class,
             RentCloudCommand.class
     );
 
---
+++ AreaShop/src/main/java/me/wiefferink/areashop/commands/util/RegionInfoUtil.java
@@ -0,0 +1,69 @@
+package me.wiefferink.areashop.commands.util;
+
+import me.wiefferink.areashop.MessageBridge;
+import me.wiefferink.areashop.managers.IFileManager;
+import me.wiefferink.areashop.regions.BuyRegion;
+import me.wiefferink.areashop.regions.GeneralRegion;
+import me.wiefferink.areashop.regions.RentRegion;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.CommandSender;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RegionInfoUtil {
+
+    public static void showRegionInfo(
+            @NonNull MessageBridge messageBridge,
+            @NonNull IFileManager fileManager,
+            @NonNull CommandSender sender,
+            @NonNull OfflinePlayer owner
+    ) {
+        // Get the regions owned by the player
+        Set<RentRegion> rentRegions = new HashSet<>();
+        for(RentRegion region : fileManager.getRentsRef()) {
+            if(region.isOwner(owner)) {
+                rentRegions.add(region);
+            }
+        }
+        Set<BuyRegion> buyRegions = new HashSet<>();
+        for(BuyRegion region : fileManager.getBuysRef()) {
+            if(region.isOwner(owner)) {
+                buyRegions.add(region);
+            }
+        }
+        // Get the regions the player is added as friend
+        Set<GeneralRegion> friendRegions = new HashSet<>();
+        for(GeneralRegion region : fileManager.getRegionsRef()) {
+            if(region.getFriendsFeature().getFriends().contains(owner.getUniqueId())) {
+                friendRegions.add(region);
+            }
+        }
+
+        // Send messages
+        boolean foundSome = !rentRegions.isEmpty() || !buyRegions.isEmpty() || !friendRegions.isEmpty();
+        if(!foundSome) {
+            messageBridge.message(sender, "me-nothing", owner.getName());
+            return;
+        }
+        messageBridge.message(sender, "me-header", owner.getName());
+        if(!rentRegions.isEmpty()) {
+            for(RentRegion region : rentRegions) {
+                messageBridge.messageNoPrefix(sender, "me-rentLine", region);
+            }
+        }
+        if(!buyRegions.isEmpty()) {
+            for(BuyRegion region : buyRegions) {
+                messageBridge.messageNoPrefix(sender, "me-buyLine", region);
+            }
+        }
+        if(!friendRegions.isEmpty()) {
+            for(GeneralRegion region : friendRegions) {
+                messageBridge.messageNoPrefix(sender, "me-friendLine", region);
+            }
+        }
+        messageBridge.messageNoPrefix(sender, "me-clickHint");
+    }
+
+}
