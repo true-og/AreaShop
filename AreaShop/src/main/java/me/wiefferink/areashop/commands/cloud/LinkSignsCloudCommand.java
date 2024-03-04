9fd8a740dbab1ad216bc6b77a6d29a65847185df
Andrew Wong <42793301+md5sha256@users.noreply.github.com>
2024-03-04 14:08:44 +1100

    Refactor linksign command to cloud

---
+++ AreaShop/src/main/java/me/wiefferink/areashop/commands/cloud/LinkSignsCloudCommand.java
@@ -0,0 +1,89 @@
+package me.wiefferink.areashop.commands.cloud;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import me.wiefferink.areashop.AreaShop;
+import me.wiefferink.areashop.MessageBridge;
+import me.wiefferink.areashop.commands.util.SignProfileUtil;
+import me.wiefferink.areashop.managers.SignLinkerManager;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.incendo.cloud.Command;
+import org.incendo.cloud.bean.CommandProperties;
+import org.incendo.cloud.context.CommandContext;
+import org.incendo.cloud.parser.flag.CommandFlag;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nonnull;
+
+@Singleton
+public class LinkSignsCloudCommand extends CloudCommandBean {
+
+    private final MessageBridge messageBridge;
+    private final SignLinkerManager signLinkerManager;
+    private final AreaShop plugin;
+
+    private final CommandFlag<String> profileFlag;
+
+    @Inject
+    public LinkSignsCloudCommand(
+            @Nonnull MessageBridge messageBridge,
+            @Nonnull SignLinkerManager signLinkerManager,
+            @Nonnull AreaShop plugin
+    ) {
+        this.messageBridge = messageBridge;
+        this.signLinkerManager = signLinkerManager;
+        this.plugin = plugin;
+        this.profileFlag = SignProfileUtil.createDefault(plugin);
+    }
+
+    @Override
+    public String stringDescription() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    protected Command.Builder<? extends CommandSender> configureCommand(@NotNull Command.Builder<CommandSender> builder) {
+        return builder.literal("linksign")
+                .permission("areashop.linksigns")
+                .senderType(Player.class)
+                .flag(this.profileFlag)
+                .handler(this::handleCommand);
+    }
+
+    @Override
+    protected @NonNull CommandProperties properties() {
+        return null;
+    }
+
+    public String getHelp(CommandSender target) {
+        if (target.hasPermission("areashop.linksigns")) {
+            return "help-linksigns";
+        }
+        return null;
+    }
+
+    private void handleCommand(@Nonnull CommandContext<Player> context) {
+        Player player = context.sender();
+        if (signLinkerManager.isInSignLinkMode(player)) {
+            signLinkerManager.exitSignLinkMode(player);
+            return;
+        }
+        // Get the profile
+        String profile = SignProfileUtil.getOrParseProfile(context, this.plugin);
+        this.signLinkerManager.enterSignLinkMode(player, profile);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
