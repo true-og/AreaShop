90e010494a6c0b89e4616010afa3c5a719b3f0af
Andrew Wong <42793301+md5sha256@users.noreply.github.com>
2024-03-18 11:25:01 +1100

    Fix bad timeunit parsing, add suggestions to DurationInputParser

--- AreaShop/src/main/java/me/wiefferink/areashop/commands/util/DurationInputParser.java
+++ AreaShop/src/main/java/me/wiefferink/areashop/commands/util/DurationInputParser.java
@@ -7,11 +7,17 @@ import org.incendo.cloud.context.CommandInput;
 import org.incendo.cloud.parser.ArgumentParseResult;
 import org.incendo.cloud.parser.ArgumentParser;
 import org.incendo.cloud.parser.ParserDescriptor;
+import org.incendo.cloud.suggestion.Suggestion;
+import org.incendo.cloud.suggestion.SuggestionProvider;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
-public class DurationInputParser<C> implements ArgumentParser<C, DurationInput> {
+public class DurationInputParser<C> implements ArgumentParser<C, DurationInput>, SuggestionProvider<C> {
 
     public static <C> ParserDescriptor<C, DurationInput> durationInputParser() {
         return ParserDescriptor.of(new DurationInputParser<>(), DurationInput.class);
@@ -32,18 +38,46 @@ public class DurationInputParser<C> implements ArgumentParser<C, DurationInput> 
             return ArgumentParseResult.failure(new AreaShopCommandException("setduration-wrongAmount", input));
         }
         String duration = input.substring(0, index);
-        String durationUnit = input.substring(index, input.length() - 1);
+        String durationUnit = input.substring(index);
         int durationInt;
         try {
             durationInt = Integer.parseInt(duration);
         } catch (NumberFormatException ex) {
+            ex.printStackTrace();
             return ArgumentParseResult.failure(new AreaShopCommandException("setduration-wrongAmount", duration));
         }
-        Optional<TimeUnit> timeUnit = DurationInput.getTimeUnit(durationUnit);
+        Optional<TimeUnit> timeUnit = DurationInput.getTimeUnit(durationUnit.toLowerCase(Locale.ENGLISH));
         if (timeUnit.isEmpty()) {
-            return ArgumentParseResult.failure(new AreaShopCommandException("setduration-wrongAmount", duration));
+            return ArgumentParseResult.failure(new AreaShopCommandException("setduration-wrongFormat", durationUnit));
         }
         DurationInput durationInput = new DurationInput(durationInt, timeUnit.get());
+        commandInput.readString();
         return ArgumentParseResult.success(durationInput);
+    }
+
+    @Override
+    public @NonNull CompletableFuture<@NonNull Iterable<@NonNull Suggestion>> suggestionsFuture(
+            @NonNull CommandContext<C> context,
+            @NonNull CommandInput input
+    ) {
+        String text = input.peekString();
+        int firstText = 0;
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if (Character.isAlphabetic(c)) {
+                firstText = c;
+                break;
+            }
+        }
+        if (firstText == 0) {
+            return CompletableFuture.completedFuture(Collections.emptyList());
+        }
+        String timeUnit = text.substring(firstText);
+        List<String> suffixes = DurationInput.SUFFIXES;
+        List<Suggestion> suggestions = suffixes.stream()
+                .filter(suffix -> suffix.startsWith(timeUnit))
+                .map(Suggestion::simple)
+                .toList();
+        return CompletableFuture.completedFuture(suggestions);
     }
 }
